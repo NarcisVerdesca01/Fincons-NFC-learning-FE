e099ab52633da4a5a3d8fab6587e0e7416d671eb
chiara.basile <chiara.basile@finconsgroup.com>
2024-03-07 15:21:07 +0100

    create the footer component

--- src/component/footer/Footer.tsx
+++ src/component/footer/Footer.tsx
@@ -1,30 +1,52 @@
 import "../footer/Footer.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-content-column">
-          <ul className="footer-menu-list">
-            <li>
-              <a href="#" className="hidden-link-text">
-                Link
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hidden-link-text">
-                Link
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hidden-link-text">
-                Link
-              </a>
-            </li>
-          </ul>
+    <div className={`row m-0 footer-style`}>
+      <div className={`footer-text`}>
+        <div className={`container`}>
+          <div className="row">
+            <div className={`col text-center`}>
+              <h4>Follow us on the social media</h4>
+              <div>
+                <p>
+                  Instagram <i className={`bi bi-instagram`}></i>
+                </p>
+
+                <p>
+                  Twitter <i className={`bi bi-twitter-x`}></i>
+                </p>
+                <p>
+                  Facebook <i className={`bi bi-facebook`}></i>
+                </p>
+              </div>
+            </div>
+            <div className={`col text-center`}>
+              <h4>Contacts</h4>
+              <div>
+                <p>
+                  Telephone <i className={`bi bi-telephone`}></i>
+                  <br></br>+39 000 000 00 00
+                </p>
+                <p>
+                  Email <i className={`bi bi-envelope`}></i>
+                  <br></br>example.example@finconsgroup.com
+                </p>
+              </div>
+            </div>
+            <div className={`col text-center`}>
+              <h4>Team</h4>
+              <div>
+                <p>
+                  NFC E-LEARNING TEAM <i className={`bi bi-people`}></i>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 export default Footer;
