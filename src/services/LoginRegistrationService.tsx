a009d295743af0ca46ec994d5898011cc4a79769
carlo.vitto <carlo.vitto@finconsgroup.com>
2024-03-20 15:59:04 +0100

    fixed services

--- src/services/LoginRegistrationService.tsx
+++ src/services/LoginRegistrationService.tsx
@@ -12,23 +12,26 @@ const REGISTRATION_TUTOR_URI = VERSION_URI + "/auth/tutor/register";
 const LOGIN_URI = VERSION_URI + "/auth/login";
 const USER_DETAILS_URI = VERSION_URI + "/email";
 const UPDATE_USER_DETAILS = VERSION_URI + "/update-user"
-const token = Cookies.get("jwt-token")
 
- const registrationStudentService = async (user: User) => {
+const registrationStudentService = async (user: User) => {
   return await axios.post(REGISTRATION_STUDENT_URI, user);
 }
 
- const registrationTutorService = async (user: User) => {
+const registrationTutorService = async (user: User) => {
+  const token = Cookies.get("jwt-token")
+
   return await axios.post(
     REGISTRATION_TUTOR_URI,
     user,
     {
-      headers: { Authorization: `Bearer ${token}`}
+      headers: { Authorization: `Bearer ${token}` }
     }
   );
 }
 
- const updateUserData = async (updatedUserDetails : UserDetailsModel) => {
+const updateUserData = async (updatedUserDetails: UserDetailsModel) => {
+  const token = Cookies.get("jwt-token")
+
   return await axios.put(
     UPDATE_USER_DETAILS,
     {
@@ -39,7 +42,7 @@ const token = Cookies.get("jwt-token")
     },
     {
       params: { email: updatedUserDetails.email },
-      headers: { Authorization: `Bearer ${token}`}
+      headers: { Authorization: `Bearer ${token}` }
     }
   )
 }
@@ -52,9 +55,12 @@ const loginService = async (loginInput: LoginUserModel) => {
   return response;
 }
 
- const getUserDetails = async () => {
+const getUserDetails = async () => {
+  const token = Cookies.get("jwt-token")
+
   const response = await axios.get(USER_DETAILS_URI, {
-    headers: { Authorization: `Bearer ${token}`}
+
+    headers: { Authorization: `Bearer ${token}` }
   });
   return response.data;
 }
