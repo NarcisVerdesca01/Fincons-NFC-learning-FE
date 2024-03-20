0b779f3b032245cfa6acc0b8ddade19846d0053e
chiara.basile <chiara.basile@finconsgroup.com>
2024-03-20 15:56:18 +0100

    new festures on profile component

--- src/component/settingsAdmin/buttonSettings/ButtonUpdateCourse.tsx
+++ src/component/settingsAdmin/buttonSettings/ButtonUpdateCourse.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import CreateLesson from '../lesson/CreateLesson';
 import UpdateCourse from '../course/UpdateCourse';
 
 
