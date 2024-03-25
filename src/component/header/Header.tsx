7537425d43019a7a9751bcd2062b51e46a2daa8b
NarcisVerdesca01 <narcis.verdesca@finconsgroup.com>
2024-03-25 11:22:10 +0100

    Design/responsivity of application

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
         console.log("sono appena uscito");
         navigate("/authentication");
         window.location.reload(); // Reload the page to ensure that the old token is removed
-      };
+    };
 
     const goToCourses = () => {
         navigate("/courses")
@@ -44,66 +44,66 @@ const Header = () => {
     const token = Cookies.get("jwt-token");
 
     useEffect(() => {
-            LoginRegistrationService.getUserDetails().then((res) => {
-                console.log(res.data.roles[0].name, "get userDetails");
-                console.log(res.data.email, "get userDetails");
-                if (res.data.roles[0].name === "ROLE_ADMIN") {
-                    console.log(res.data.roles[0].name, "getUserDetails res.data.roles[0].name");
-                    setAdminNavBar(true);
-                } else if (res.data.roles[0].name === "ROLE_TUTOR") {
-                    setTutorNavBar(true);
-                }
-            });
-        
+        LoginRegistrationService.getUserDetails().then((res) => {
+            console.log(res.data.roles[0].name, "get userDetails");
+            console.log(res.data.email, "get userDetails");
+            if (res.data.roles[0].name === "ROLE_ADMIN") {
+                console.log(res.data.roles[0].name, "getUserDetails res.data.roles[0].name");
+                setAdminNavBar(true);
+            } else if (res.data.roles[0].name === "ROLE_TUTOR") {
+                setTutorNavBar(true);
+            }
+        });
+
     });
 
     return (
         <div className={`bodyHeader`}>
             <div className={`firstComponentHeader`}>
                 <div className={`containerTitleHeader`}>
-                    <h4 className={`titleHeader`}>NFC E-Learning web site</h4>
+                    <h4 className={`titleHeader lead`}>NFC E-Learning web site</h4>
                 </div>
                 <div className={`navbarHeader`}>
                     <div className={`containerNavBarHeader`}>
                         <div className={`containerButtonNavBar`}>
                             <button className={`buttonNavBar`} onClick={goToHomePage}>
-                                <p className={`nameButton`}>Home Page</p>
+                                <p className={`nameButton lead`}>Home Page</p>
                             </button>
                         </div>
                         <div className={`containerButtonNavBar`}>
                             <button className={`buttonNavBar`} onClick={goToCourses}>
-                                <p className={`nameButton`}>Courses</p>
+                                <p className={`nameButton lead`}>Courses</p>
                             </button>
                         </div>
                         <div className={`containerButtonNavBar`}>
                             <button className={`buttonNavBar`} onClick={goToPageDedicatedCourses}>
-                                <p className={`nameButton`}>My courses</p>
+                                <p className={`nameButton lead`}>Our courses</p>
                             </button>
                         </div>
                         {tutorNavBar && (
                             <div className={`containerButtonNavBar`}>
                                 <button className={`buttonNavBar`} onClick={goToSettingsTutor}>
-                                    <p className={`nameButton`}>Settings</p>
+                                    <p className={`nameButton lead`}>Settings</p>
                                 </button>
                             </div>
                         )}
                         {adminNavBar && (
                             <div className={`containerButtonNavBar`}>
                                 <button className={`buttonNavBar`} onClick={goToSettingsAdmin}>
-                                    <p className={`nameButton`}>Settings</p>
+                                    <p className={`nameButton lead`}>Settings</p>
                                 </button>
                             </div>
                         )}
                         <div className={`containerButtonNavBar`}>
                             <button className={`buttonNavBar`} onClick={handleLogout}>
-                                <p className={`nameButton`}>Logout</p>
+                                <p className={`nameButton lead`}>Logout</p>
                             </button>
                         </div>
                         <div className={`containerButtonNavBar`}>
                             {/*<button className={`buttonNavBar`} onClick={goToProfile}>
                                 <p className={`nameButton`}>Profile</p>
                         </button>*/}
-                        <p className={`nameButton`}><ButtonProfile /></p>
+                            <p className={`nameButton`}><ButtonProfile /></p>
                         </div>
                     </div>
 
